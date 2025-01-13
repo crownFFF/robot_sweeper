b004748a30b3c164d8e63995617a0c1bdd9ab8db
crownFFF <asd8792323@gmail.com>
2025-01-13 21:43:47 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['three'],
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // 禁止服务器端渲染时使用与浏览器相关的库
+      config.node = {
+        fs: 'empty',
+        module: 'empty',
+      };
+    }
+    return config;
   },
 };
-
-export default nextConfig;
