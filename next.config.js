acbbe02ba865767711ae6caedcf9ee066e9b778f
crownFFF <asd8792323@gmail.com>
2025-01-13 22:18:45 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output:'export',
   transpilePackages: ['three'],
+  experimental:{
+    appDir:true
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig
